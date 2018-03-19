617abf489adaef796671eb6ebf616949933a42bd
Anastaciya Gourianova <gourianova91@gmail.com>
2018-03-19 15:33:18 +0300

    Add Singleton Driver Initialization

---
+++ TestFramework/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestFramework
+{
+    public class Driver
+    {
+        private static readonly Lazy<Driver> lazy =
+        new Lazy<Driver>(() => new Driver());
+
+        public string Name { get; private set; }
+
+        private Driver()
+        {
+            Name = System.Guid.NewGuid().ToString();
+        }
+
+        public static Driver GetDriver()
+        {
+            return lazy.Value;
+        }
+    } 
+}
