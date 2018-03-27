bc9acf4276098446acd09e21e66fc42110f4b035
Anastaciya Gourianova <gourianova91@gmail.com>
2018-03-27 22:50:48 +0300

    Edit Test for Yandex Page

--- TestFramework/Pages/YandexPage.cs
+++ TestFramework/Pages/YandexPage.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace TestFramework
 {
@@ -8,27 +9,44 @@ namespace TestFramework
     {
         public static By geolocation = By.CssSelector("div.col.headline__item.headline__leftcorner > a");
         public static By city = By.CssSelector("#city__front-input");
-        public static By more = By.CssSelector("a.home-link.home-link_blue_yes.home-tabs__link.home-tabs__more-switcher.dropdown-menu.dropdown-menu__switcher.i-bem.dropdown-menu_js_inited.dropdown-menu_action_closed");
-        public static By moreList = By.CssSelector("a.home-link.home-link_blue_yes.home-tabs__link.home-tabs__more-switcher.dropdown-menu.dropdown-menu__switcher.i-bem.dropdown-menu_js_inited.dropdown-menu_action_open");
+        public static By cityList = By.CssSelector("ul.popup__items.input__popup-items");
+        public static By more = By.CssSelector("a.home-link.home-link_blue_yes.home-tabs__link.home-tabs__more-switcher");
+        public static By moreList = By.CssSelector("div.home-tabs__more");
+        public static By checkbox = By.CssSelector("input.checkbox__control");
 
         public void compareContent(string cityLondon, string cityParis)
         {
+            Waiter wait = new Waiter();
             driver.FindElement(geolocation).Click();
+
+            driver.FindElement(checkbox).Click();
             driver.FindElement(city).Clear();
             driver.FindElement(city).SendKeys(cityLondon);
-            driver.FindElement(city).SendKeys(Keys.Enter);
+            wait.isDisplayed(cityList);
+            driver.FindElement(city).SendKeys(Keys.ArrowDown + Keys.Enter);
+
+            wait.isDisplayed(more);
             driver.FindElement(more).Click();
-            Waiter wait = new Waiter();
-            Assert.True(wait.isDisplayed(moreList));
-            IList<IWebElement> moreListForLondon = driver.FindElements(moreList);
+            wait.isDisplayed(moreList);
+            string moreListLondon = driver.FindElement(moreList).Text;
+
             driver.FindElement(more).Click();
-            Assert.False(wait.isDisplayed(moreList));
+            wait.isDisplayed(moreList);
             driver.FindElement(geolocation).Click();
+
             driver.FindElement(city).Clear();
             driver.FindElement(city).SendKeys(cityParis);
-            driver.FindElement(city).SendKeys(Keys.Enter);
+            wait.isDisplayed(cityList);
+            driver.FindElement(city).SendKeys(Keys.ArrowDown + Keys.Enter);
+
+            wait.isDisplayed(more);
+
+            Assert.AreEqual(cityParis, driver.FindElement(geolocation).Text);
+
             driver.FindElement(more).Click();
-            IList<IWebElement> moreListForParis = driver.FindElements(moreList);
+            string moreListParis = driver.FindElement(moreList).Text;
+
+            Assert.True(moreListLondon.Equals(moreListParis));
         } 
     }
 }
